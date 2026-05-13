c7c471d3c246e6e2bb08fa396a59296ee024d657
slayne0 <majfeuguicheteau@gmail.com>
2026-05-13 11:58:45 +0200

    accueil-html

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,6 +1,37 @@
-<h1>TECH QUIZZ</h1>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <h1>TECH QUIZZ</h1>
 <p><a href="/quizzes">Voir les quizz</a></p>
 <p><a href="/users">Voir les utilisateurs</a></p>
 <p><a href="/questions">Voir les questions</a></p>
 <p><a href="/responses">Voir les réponses</a></p>
+
+
+<h1>Login to Account</h1>
+<p class="under-title">Please enter your email and password to continue</p>
+<form action="" method="post">
+    <div class="label-wrapper">
+    <label for="email">Email address:</label>
+    </div>
+    <input type="text" placeholder="esteban_schiller@gmail.com">
+
+    <div class="label-wrapper">
+    <label for="email">Password</label>
+    <a class="forget-password" href="">Forget Password?</a>
+    </div>
+    <input type="text" placeholder="* * * * *">
+    <div class="remenber-password-check-wrapper">
+        <p class="remenber-password-text">Remenber Password</p>
+    </div>
+    <button class="button-sign-in" type="submit">Sign In</button>
+</form>
+<p class="create-account-text">Don't have an account? <a class="link-create-account" href="">Create Account</a></p>
+</body>
+</html>
