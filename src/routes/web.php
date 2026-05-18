039d6430ebc253de72eb81d1a34253fd84f7ab21
unknown <nseck@edenschool.fr>
2026-05-18 11:58:34 +0200

    Refactor routes to use AuthController and AnswerController; added signout route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuizzController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\QuestionController;
-use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\AnswerController;
 
 Route::get("/", [QuizzController::class, 'index']);
 Route::get("/quizzes", [QuizzController::class, 'index'])->name('quizzes');
 Route::get("/quizzes/{id}", [QuizzController::class, 'showOneQuizz'])->name('quizzes.show');
 Route::get("/login", [AuthController::class, 'login'])->name('login');
 Route::get("/signup", [AuthController::class, 'signup'])->name('signup');
+Route::get("/signout", [AuthController::class, 'signout'])->name('signout');
