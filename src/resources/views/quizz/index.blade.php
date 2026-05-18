2c03c75c262e442145d45d5c7b7086958497824e
slayne0 <majfeuguicheteau@gmail.com>
2026-05-18 10:52:48 +0200

    css-page-welcome

--- src/resources/views/quizz/index.blade.php
+++ src/resources/views/quizz/index.blade.php
@@ -1,6 +1,6 @@
 <h1>QUIZZES</h1>
 
-@if ($quizzes->isEmpty())
+<!-- @if ($quizzes->isEmpty())
 <p>Aucun quizz disponible.</p>
 @else
 <ul>
@@ -13,8 +13,34 @@
     </li>
     @endforeach
 </ul>
-@endif
+@endif -->
 
-<p><a href="/">Retour à l'accueil</a></p>
+<!-- <p><a href="/">Retour à l'accueil</a></p>
 <p><a href="/questions">Voir les questions</a></p>
-<p><a href="/users">Voir les utilisateurs</a></p>
+<p><a href="/users">Voir les utilisateurs</a></p> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="welcome-page-wrapper">
+    <div class="title-welcome-wrapper">
+        <h1 class="welcome">Welcome to the <br><span>Fronted Quiz!</span></h1>
+        <p>Pick a subject to get started.</p>   
+    </div>
+    <div class="link-wrapper">
+        <ul>
+            <li><a href="quizzes?id=1">HTML</a></li>
+            <li><a href="quizzes?id=2">CSS</a></li>
+            <li><a href="quizzes?id=3">Javascript</a></li>
+            <li><a href="quizzes?id=4">Accessibility</a></li>
+        </ul>
+    </div>
+    </div>
+</body>
+</html>
