c18f20dbbe6f9f0087edc0df3910bdcd7ab9d439
slayne0 <majfeuguicheteau@gmail.com>
2026-05-20 09:29:35 +0200

    css-welcome-page

--- src/resources/views/quizz/index.blade.php
+++ src/resources/views/quizz/index.blade.php
@@ -1,19 +1,6 @@
-<h1>QUIZZES</h1>
+<!-- <h1>QUIZZES</h1> -->
 
-<!-- @if ($quizzes->isEmpty())
-<p>Aucun quizz disponible.</p>
-@else
-<ul>
-    @foreach ($quizzes as $quizz)
-    <li>
-        @if ($quizz->logo_url)
-        <img src="{{ $quizz->logo_url }}" alt="Logo {{ $quizz->title }}" style="max-width: 120px;">
-        @endif
-        <strong>{{ $quizz->title }}</strong>
-    </li>
-    @endforeach
-</ul>
-@endif -->
+
 
 <!-- <p><a href="/">Retour à l'accueil</a></p>
 <p><a href="/questions">Voir les questions</a></p>
@@ -35,10 +22,28 @@
     </div>
     <div class="link-wrapper">
         <ul>
-            <li><a href="quizzes?id=1">HTML</a></li>
-            <li><a href="quizzes?id=2">CSS</a></li>
-            <li><a href="quizzes?id=3">Javascript</a></li>
-            <li><a href="quizzes?id=4">Accessibility</a></li>
+
+        @if ($quizzes->isEmpty())
+<p>Aucun quizz disponible.</p>
+@else
+<ul>
+    @foreach ($quizzes as $quizz)
+    <li>
+        @if ($quizz->logo_url)
+        <div>
+            <img class="logo-quizz" src="{{ $quizz->logo_url }}" alt="Logo {{ $quizz->title }}" style="max-width: 120px;">
+            <strong>{{ $quizz->title }}</strong>
+        </div>
+        @endif
+    </li>
+    @endforeach
+</ul>
+@endif
+
+            <!-- <li><a href="quizzes1">HTML</a></li>
+            <li><a href="quizzes2">CSS</a></li>
+            <li><a href="quizzes3">Javascript</a></li>
+            <li><a href="quizzes4">Accessibility</a></li> -->
         </ul>
     </div>
     </div>
